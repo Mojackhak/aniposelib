ed75a70487c6fd67589c43c2b6b6df1787b04395
Ankang Hu <23111220065@m.fudan.edu.cn>
2024-09-21 05:15:23 +0800

    Update boards.py

--- aniposelib/boards.py
+++ aniposelib/boards.py
@@ -589,7 +589,7 @@ class CharucoBoard(CalibrationObject):
         return np.copy(self.empty_detection)
 
     def draw(self, size):
-        return self.board.draw(size)
+        return self.board.generateImage(size) # opencv 4.10.0
 
     def fill_points(self, corners, ids):
         out = self.get_empty_detection()
